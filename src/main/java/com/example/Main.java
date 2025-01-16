d722ad57cc735d71c33a8569ea8fce952668f3ae
tuan <pro03tuan@gmail.com>
2025-01-16 12:42:55 +0700

    lai

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,8 +3,5 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!"); 
-        System.out.println("Hello world!");
     }
 }
