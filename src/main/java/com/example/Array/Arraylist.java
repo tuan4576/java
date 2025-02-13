f48df6abdda816e77fe8ee28ad2d8d316f0757b8
tuan <pro03tuan@gmail.com>
2025-02-13 21:41:16 +0700

    ArrayMethod

--- src/main/java/com/example/Array/Arraylist.java
+++ src/main/java/com/example/Array/Arraylist.java
@@ -3,7 +3,15 @@ import java.util.ArrayList;
 public class Arraylist {
     public void Arr(){
         ArrayList<String> card = new ArrayList<>();
-        card.add("Them");
-        System.out.println(card);
+        card.add("Hai");
+        card.add("Ba");
+        card.add("Bon");
+        card.add(0,"Mot");
+        card.set(0,"Nam");
+        card.remove(0);
+        card.clear();
+        // System.out.println(card);
+        // System.out.println("GetIndex : " + card.get(0));
+        System.out.println("Setvalue : " + card);
     }
 }
